4854f32e6939d0cfff09143a957b8ec1cd778b44
nakuljn <62203669+nakuljn@users.noreply.github.com>
2025-11-24 08:31:16 +0530

    feat: added ec2 url as allowed hosts

--- src/config.py
+++ src/config.py
@@ -40,7 +40,7 @@ class AppConfig:
     # CORS Configuration
     CORS_ALLOWED_ORIGINS: list = os.getenv(
         "CORS_ALLOWED_ORIGINS",
-        "http://localhost:3000,http://localhost:7860,http://localhost:5173,https://parkho-ai-frontend-ku7bn6e62q-uc.a.run.app,https://parkho-ai-frontend-846780462763.us-central1.run.app,https://ai-content-tutor-ku7bn6e62q-uc.a.run.app,https://ai-content-tutor-846780462763.us-central1.run.app,http://3.107.3.42:3000,http://3.107.3.42:8080"
+        "http://localhost:3000,http://localhost:7860,http://localhost:5173,https://parkho-ai-frontend-ku7bn6e62q-uc.a.run.app,https://parkho-ai-frontend-846780462763.us-central1.run.app,https://ai-content-tutor-ku7bn6e62q-uc.a.run.app,https://ai-content-tutor-846780462763.us-central1.run.app,http://13.236.51.35:3000,http://13.236.51.35:8080"
     ).split(",")
 
 class RerankingConfig:
